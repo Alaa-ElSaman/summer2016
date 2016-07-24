97b693c6338e37980490f64c910155e351b97e19
Alaa ElSaman <alaa.elsaman@gmail.com>
2016-07-24 18:20:19 +0200

    initial project upload

---
+++ WebRestful_Project2/src/org/service/rest/RestfulApp.java
@@ -0,0 +1,20 @@
+package org.service.rest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RestfulApp extends javax.ws.rs.core.Application{
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+		
+		
+		
+		return classes;
+	}
+	
+	
+
+}
